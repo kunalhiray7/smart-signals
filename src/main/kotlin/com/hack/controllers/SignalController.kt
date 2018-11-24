3463723bf112a2020bb42dc93fa997b5293693b8
Kunal Hire <kunal.hire@hexad.de>
2018-11-24 23:21:27 +0100

    added cors filter

--- src/main/kotlin/com/hack/controllers/SignalController.kt
+++ src/main/kotlin/com/hack/controllers/SignalController.kt
@@ -5,11 +5,9 @@ import com.hack.models.microProcessor.PostSensorDataRequest
 import com.hack.services.SignalProcessingService
 import com.hack.services.TrafficDataService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["http://localhost:3000"], maxAge = 3000)
 @RestController
 class SignalController(private val signalProcessingService: SignalProcessingService) {
 
